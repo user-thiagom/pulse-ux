10e30bd5b77a55b821d837b0415289e9b585a42f
Thiago Fernandes <thiagomateus123@hotmail.com>
2026-05-07 12:38:42 -0300

    Alimentando a aplicação com o contexto global

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SurveyProvider } from './context/SurveyContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SurveyProvider>
+      <App />
+    </SurveyProvider>
   </StrictMode>,
 )
